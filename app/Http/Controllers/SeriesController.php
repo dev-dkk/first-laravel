794622ba0bd53e10d84f99beacb96288b46ecc91
Dérick Santos <santos.c.derick@gmail.com>
2024-10-17 11:33:11 -0300

    Criando componente para armazenar layout padra para viwes

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -16,55 +16,7 @@ class SeriesController extends Controller
             'The Walking Dead',
             'Breaking Bead'
         ];
-        return view('series.index', compact('series'));
+        return view('series.index')-> with('series', $series);
 
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
