43fc268562e5ff2a7b2f3885099fe9cff3c4c167
Dérick Santos <santos.c.derick@gmail.com>
2024-10-19 08:49:03 -0300

    Não lembro o que eu fiz de mudança

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout title="Nova Serie">
     <form action="" method="post" >
-        <label for="nome">Nome</label>
-        <input id="nome" name="nome" type="text" placeholder="Insira uma nova serie">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input id="nome" name="nome" type="text" class="form-control" placeholder="Insira uma nova serie">
+        </div>
+        <button class="btn btn-primary" type="submit">Adicionar</button>
     </form>
     </form>
 </x-layout>
